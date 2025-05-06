213dd73bba4f1bfcb02bd5980aaae73ceb8b8377
adam-hehe <126042471+adam-hehe@users.noreply.github.com>
2025-05-05 21:36:24 -0700

    Finished explore section and implemented as instructed

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,40 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelector = document.getElementById('horn-select');
+  const hornImage = document.querySelector('#expose img');
+  const hornAudio = document.querySelector('audio');
+  const volumeSlider = document.getElementById('volume');
+  const volumeImg = document.querySelector('#volume-controls img');
+  const button = document.querySelector('button');
+  const showConfetti = new JSConfetti();
+
+  hornSelector.addEventListener('input', () => { 
+    hornImage.src = `./assets/images/${hornSelector.value}.svg`;
+    hornAudio.src = `./assets/audio/${hornSelector.value}.mp3`;
+  });
+
+  volumeSlider.addEventListener('input', () => {
+    const volumeValue = Number(volumeSlider.value)
+    let currentLevel;
+
+    if(volumeValue >= 67) {
+      currentLevel = 3;
+    }else if(volumeValue >= 33) {
+      currentLevel = 2;
+    }else if(volumeValue >= 1) {
+      currentLevel = 1
+    }else {
+      currentLevel = 0;
+    }
+
+    volumeImg.src = `assets/icons/volume-level-${currentLevel}.svg`;
+    hornAudio.volume = volumeValue / 100;
+  });
+
+  button.addEventListener("click", () => {
+    hornAudio.currentTime = 0;
+    showConfetti.addConfetti()
+    hornAudio.play();
+  });
 }
